7f2c528a1d27f45c1b2e9b626e91004fa08054a4
Ema <oudin.ema@gmail.com>
2025-10-23 10:55:04 +0200

    Création du projet

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./*.html", 
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./dist/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
